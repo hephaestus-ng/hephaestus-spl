ceda65fabfe0c660bb3319a135dc41fa6de2a442
Rodrigo Bonifacio <rbonifacio123@gmail.com>
2018-04-13 14:36:56 -0300

    fix the stack build

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,9 +1,7 @@
+module Main where 
+
 import Control.Monad
 import Test.HUnit.Base
 
-main :: IO Test.HUnit.Base.Counts
--- main = runFeatureTests
-main = do
-    a    <-
-    -- d    <- runXMLParserTests
-    return
+main :: IO ()
+main = print "hello"
